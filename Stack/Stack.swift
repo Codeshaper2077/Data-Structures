c080157e08b00532330de00c287102de13981cec
Ivan <codeshaper2077@yandex.ru>
2022-08-17 10:58:15 -0700

    modified stack

--- Stack/Stack.swift
+++ Stack/Stack.swift
@@ -1,5 +1,40 @@
 import Foundation
 
 struct Stack<T> {
+	fileprivate var array = [T]()
 	
+	public var count: Int {
+		return array.count
+	}
+	
+	public var isEmpty: Bool {
+		return array.isEmpty
+	}
+	
+	public mutating func push(_ element: T) {
+		array.append(element)
+	}
+	
+	public mutating func pop() -> T? {
+		array.popLast()
+	}
+	
+	public var top: T? {
+		return array.last
+	}
+		
 }
+
+var object = Stack<Int>()
+object.push(1)
+object.push(2)
+object.push(3)
+print(object)
+
+object.pop()
+object.pop()
+object.pop()
+
+print(object.top)
+
+print(object)
